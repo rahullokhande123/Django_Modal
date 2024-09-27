41c18c8b7ba0f9f700952389cf4155dd56baa8a1
Rahul Lokhande <rlokhande06244@gmail.com>
2024-09-27 17:59:23 +0530

    Modal

--- project/app/views.py
+++ project/app/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .models import Student
 
 # Create your views here.
+def home(request):
+    return render(request,'home.html')
+
+def login(request):
+    return render(request,'login.html')
