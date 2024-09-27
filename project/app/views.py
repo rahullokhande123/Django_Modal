555af06f7eaecc70acbdddba5eb3214423a5415c
Rahul Lokhande <rlokhande06244@gmail.com>
2024-09-27 21:48:41 +0530

    Modal

--- project/app/views.py
+++ project/app/views.py
@@ -5,5 +5,18 @@ from .models import Student
 def home(request):
     return render(request,'home.html')
 
+def register(request):
+    if request.method=="POST":
+        name=request.POST.get('name')
+        email=request.POST.get('email')
+        contact=request.POST.get('contact')
+        password=request.POST.get('password')
+
+        Student.objects.create(stu_name=name,stu_email=email,stu_contact=contact,stu_password=password)
+        msg="DATA SUCCESSFULLY SUBMITED"
+        return render(request,'home.html',{'msg':msg})
+    else:
+        return render(request,'register.html')
+
 def login(request):
     return render(request,'login.html')
