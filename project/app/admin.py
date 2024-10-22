a330748cb46a24c897ec36192e3b524ada63d3df
Rahul Lokhande <rlokhande06244@gmail.com>
2024-10-22 18:05:26 +0530

    Modals

--- project/app/admin.py
+++ project/app/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Aadhar,User,Department
 
 # Register your models here.
+
+admin.site.register(Aadhar)
+admin.site.register(User)
+admin.site.register(Department)
+
--- project/app/models.py
+++ project/app/models.py
@@ -16,3 +16,23 @@ class Query(models.Model):
 
     def __str__(self):
         return self.query
+    
+
+class Aadhar(models.Model):
+    aadhar=models.IntegerField(primary_key=True)
+    def __str__(self):
+        return str(self.aadhar)
+    
+class Department(models.Model):
+    dep_name=models.CharField(max_length=50)
+    dep_des=models.TextField()
+    def __str__(self):
+        return str(self.dep_name)
+
+
+class User(models.Model):
+    name=models.CharField(max_length=50)
+    email=models.EmailField()
+    contact=models.IntegerField()
+    aadhar_no=models.OneToOneField(Aadhar,on_delete=models.CASCADE)
+    dep_name=models.ForeignKey(Department,on_delete=models.CASCADE,null=True)
